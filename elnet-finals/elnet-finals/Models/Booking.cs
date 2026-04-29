890d2683bc8621c5ec0ff5647ab25882a076396b
cali4life1234567890-lang <cali4life1234567890@gmail.com>
2026-04-29 23:11:15 +0800

    model booking.cs added

--- elnet-finals/elnet-finals/Models/Booking.cs
+++ elnet-finals/elnet-finals/Models/Booking.cs
@@ -2,5 +2,19 @@
 {
     public class Booking
     {
+        public int Id { get; set; }
+
+        public string PickupLocation { get; set; }
+        public string Destination { get; set; }
+        public DateTime TripDate { get; set; }
+
+        public int Adults { get; set; }
+        public int Children { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public int VehicleId { get; set; }
     }
 }
