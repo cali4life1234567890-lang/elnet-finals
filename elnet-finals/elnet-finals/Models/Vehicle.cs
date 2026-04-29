bcdc592e54f057c7aff910d59e90df5bd4c3fe41
cali4life1234567890-lang <cali4life1234567890@gmail.com>
2026-04-29 22:58:48 +0800

    model vehicle.cs - added

--- elnet-finals/elnet-finals/Models/Vehicle.cs
+++ elnet-finals/elnet-finals/Models/Vehicle.cs
@@ -2,5 +2,12 @@
 {
     public class Vehicle
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public decimal PricePerHour { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
     }
 }
