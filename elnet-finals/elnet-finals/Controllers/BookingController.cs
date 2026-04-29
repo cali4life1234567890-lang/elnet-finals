bf71b11bd774d0ef9970898b56b156eee4eb70b2
cali4life1234567890-lang <cali4life1234567890@gmail.com>
2026-04-29 23:47:01 +0800

    added FleetController

--- elnet-finals/elnet-finals/Controllers/BookingController.cs
+++ elnet-finals/elnet-finals/Controllers/BookingController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using elnet_finals.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace elnet_finals.Controllers
 {
     public class BookingController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Create(int vehicleId)
         {
+            ViewBag.VehicleId = vehicleId;
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Booking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save to DB later
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(booking);
         }
     }
 }
