484df7493c0d8663219f44e572e17fb6287a581f
ajayseee <aljaycris33@gmail.com>
2026-05-07 22:37:39 +0800

    build: prepare database context and initial schema for Vehicles and Bookings

--- elnet-finals/elnet-finals/Models/Vehicle.cs
+++ elnet-finals/elnet-finals/Models/Vehicle.cs
@@ -1,13 +1,25 @@
-﻿namespace elnet_finals.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace elnet_finals.Models
 {
     public class Vehicle
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Vehicle name is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Vehicle type is required")]
         public string Type { get; set; }
+
         public int Capacity { get; set; }
+
+        [Range(0, 10000, ErrorMessage = "Price per hour must be between 0 and 10,000")]
         public decimal PricePerHour { get; set; }
+
         public string ImageUrl { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
     }
 }
