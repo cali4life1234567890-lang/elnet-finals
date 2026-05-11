a2ca0707c5d8b00d9454270ec102f456eb4603e9
cali4life1234567890-lang <cali4life1234567890@gmail.com>
2026-05-11 23:56:55 +0800

    created db context

---
+++ elnet-finals/elnet-finals/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace elnet_finals.Data
+{
+    using elnet_finals.Models;
+    using LuxBus.Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
